23959f58c68ef4944f08cb5ccd2a72e831c066ff
luckyz888 <13424298823@163.com>
2026-06-06 17:38:01 +0800

    fix: remove unused variables to fix TS6133 build errors

--- src/components/CodeEditor.tsx
+++ src/components/CodeEditor.tsx
@@ -44,7 +44,7 @@ export default function CodeEditor({ code, onCodeChange, height = '350px', datas
     monaco.languages.registerCompletionItemProvider('python', {
       provideCompletionItems: (model: any, position: any) => {
         const word = model.getWordUntilPosition(position);
-        const range = {
+        const _range = {
           startLineNumber: position.lineNumber,
           endLineNumber: position.lineNumber,
           startColumn: word.startColumn,
